5eb210db70d29621abb7009bf87198efacc30001
KosukeMiyazato <kosukem.career@gmail.com>
2025-09-14 18:10:19 +0900

    Updated package-lock.json

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,16 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      crypto: 'crypto-browserify',
+      stream: 'stream-browserify',
+      buffer: 'buffer',
+    },
+  },
+  define: {
+    global: 'globalThis',
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
