7dff6fe7fac7943a9535ae01bf8390a427ef505c
KosukeMiyazato <kosukem.career@gmail.com>
2025-09-14 18:11:09 +0900

    Fix Buffer is not defined error

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,13 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { Buffer } from 'buffer';
 import App from './App.tsx';
 import './index.css';
 import { LanguageProvider } from './contexts/LanguageContext';
 import { AuthProvider } from './contexts/AuthContext';
+
+// Polyfill Buffer for browser environment
+window.Buffer = Buffer;
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
